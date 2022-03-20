fc013911de948748325201b27241e1e70801368f
Laurent <magrez0laurent@gmail.com>
2022-03-20 09:00:37 +0100

    burger navbar anim & js

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,21 @@
 "use strict";
 
-    
+const posArray = [
+  "js-menu-open-pos0",
+  "js-menu-open-pos1",
+  "js-menu-open-pos2",
+  "js-menu-open-pos3"
+];
+
 const burgerChange = document.querySelector(".burger-button");
-const navBar = document.querySelector(".mainNav")
-    
-burgerChange.addEventListener("click", function() {
+const liList = document.querySelectorAll(".menu-general-a");
+
+burgerChange.addEventListener("click", function () {
   // e.preventDefault();
-  burgerChange.classList.toggle('js-burgerButtonChange');
-  navBar.classList.toggle('js-navbarOpen');
-}) 
-  
+  burgerChange.classList.toggle("js-burgerButtonChange");
+
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].classList.toggle("js-menu-open");
+    liList[i].classList.toggle(posArray[i]);
+  }
+});
