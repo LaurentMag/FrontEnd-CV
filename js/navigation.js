f669194bc5fe7e2e6776e524d7f950ff131e39ea
Laurent <magrez0laurent@gmail.com>
2022-03-20 20:22:01 +0100

    finished menu animation, started project part

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,13 @@
 "use strict";
 
-const posArray = [
-  "js-menu-open-pos0",
-  "js-menu-open-pos1",
-  "js-menu-open-pos2",
-  "js-menu-open-pos3"
-];
+const posArray = ["js-menu-open-pos0", "js-menu-open-pos1", "js-menu-open-pos2", "js-menu-open-pos3"];
 
 const burgerChange = document.querySelector(".burger-button");
 const liList = document.querySelectorAll(".menu-general-a");
 
-burgerChange.addEventListener("click", function () {
-  // e.preventDefault();
+burgerChange.addEventListener("click", function (e) {
+  e.stopPropagation();
+  e.preventDefault();
   burgerChange.classList.toggle("js-burgerButtonChange");
 
   for (let i = 0; i < liList.length; i++) {
